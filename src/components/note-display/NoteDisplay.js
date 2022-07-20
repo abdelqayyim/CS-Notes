3c56f9f168213b6055059f899652d9f1a232f5ea
abdelqayyim <abdelymaster@gmail.com>
2022-07-19 22:38:46 -0400

    Version 0.0.0
    implemented the syntax highlighting, need to fix scroll and channging languages

--- src/components/note-display/NoteDisplay.js
+++ src/components/note-display/NoteDisplay.js
@@ -1,10 +1,31 @@
-import React, {useContext} from 'react'; 
+import React, {useContext, useRef} from 'react'; 
 import Note from '../note/Note';
 import './NoteDisplay.css';
 import { AppProvider } from '../../AppContext';
+import Prism from "prismjs";
+import "../Themes/prism.css";
 
 const NoteDisplay = (props) => {
-    const curr = useContext(AppProvider)
+    const curr = useContext(AppProvider);
+    let note = useRef();
+    let result = useRef();
+    
+
+    setTimeout(() => {
+        Prism.highlightAll();
+    },1000)
+
+    const change = ()=>{
+        result.current.innerHTML = note.current.value.replace(new RegExp("&", "g"), "&").replace(new RegExp("<", "g"), "<");;
+        Prism.highlightAll();
+    }
+    const syncScroll = (element)=>{
+        // note.current.scrollTop = element.current.scrollTop;
+        // note.current.scrollLeft = element.current.scrollLeft;
+        result.current.scrollIntoView();
+        note.current.scrollIntoView();
+        console.log("Hello")
+    }
 
     if (curr.languageClicked && !curr.noteClicked) {
         return (
@@ -16,6 +37,7 @@ const NoteDisplay = (props) => {
         )
     }
     else if (curr.languageClicked && curr.noteClicked) {
+        const codeString = 'def Hello(): \n\tprint("Hello world")';
         return (
             <div className='clickedNote-container'>
                 <div className='note-section'>
@@ -26,12 +48,20 @@ const NoteDisplay = (props) => {
                         {curr.currNoteDescription.length === 0 && "No Description"}
                     { curr.currNoteDescription}
                     </div>
-                    <textarea className="code">
-                        {curr.currNoteDetail}
-                    </textarea>
-                    {/* <div className='code'>
-                        {curr.currNoteDetail}
-                    </div> */}
+
+                    <div className='code'>
+                        <textarea spellCheck={false} ref={note} onInput={() => { change(); syncScroll(this) }} onScroll={() => { syncScroll(this) }} className="textarea">
+
+                        </textarea>
+                        <pre  className="language-javascript script">
+                            <code ref={result} className="highlight">
+                                
+                            </code>
+                        </pre>
+                    </div>
+                    
+                    
+                    
                 </div>
                 <div className='note-suggestion'>
                 {curr.currNotes.map(note => 
@@ -41,6 +71,6 @@ const NoteDisplay = (props) => {
             </div>
         )
     }
-//Hello
 };
 export default NoteDisplay;
+        // )https://prismjs.com/download.html#themes=prism-okaidia&languages=markup+css+clike+javascript+aspnet+c+csharp+cpp+fsharp+git+go+graphql+java+javadoc+javadoclike+markdown+markup-templating+mongodb+php+python+jsx+regex+ruby+rust+sql+swift+typescript&plugins=line-numbers+highlight-keywords+command-line
